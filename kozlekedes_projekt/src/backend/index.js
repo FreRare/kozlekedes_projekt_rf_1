70cd03d7128b6ad67a26a5bcb6dd06a134036b47
Rück Richárd <h163500@stud.u-szeged.hu>
2022-12-01 19:46:35 +0100

    Járat hozzáadás backend kész tesztelve nincs még

--- kozlekedes_projekt/src/backend/index.js
+++ kozlekedes_projekt/src/backend/index.js
@@ -56,28 +56,27 @@ const CONTROL = new Control();
              response.end();
          }
          else{
-             response.json({success: false, error:"Törlés nem sikerült!"});
+             response.json({success: false, error:"Járat törlés nem sikerült!"});
              response.end();
          }
      }).catch(e=>console.log(e));
  });
 
-/*
  router.post('/api/createService', (req, response)=>{
-     const id = req.body.id;
-     const
-     CONTROL.createService(id).then((result)=>{
-         if(result){
+     const serviceNumber = req.body.serviceNumber;
+     const serviceType = req.body.serviceType;
+     const stops = req.body.stops;
+     CONTROL.createService(serviceNumber, serviceType, stops).then(res=>{
+         if(res){
              response.json({success: true});
              response.end();
          }
          else{
-             response.json({success: false, error:"Törlés nem sikerült!"});
+             response.json({success: false, error:"Járat hozzáadás nem sikerült!"});
              response.end();
          }
      }).catch(e=>console.log(e));
  });
-*/
 
  router.get('/api/profile', (req, res)=>{
      setTimeout(()=>{
@@ -201,7 +200,7 @@ const CONTROL = new Control();
     CONTROL.createNews(category, title, description, publishDate).then(res=>{
         console.log("control createNews", res);
         if(res){
-            response.json({success: true});
+            response.json({success: true, news: res});
             response.end();
         }else{
             response.json({success: false, error:"Nem megfelelő news!"});
